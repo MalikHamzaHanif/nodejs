679efc19c4a719b86db801ac7d529aa39b7c0930
Hamza Hanif <www.musamalik9@gmail.com>
2025-02-27 04:19:59 +0500

    async pattrens and  promises

---
+++ 10_async_pattrens_promises.js
@@ -0,0 +1,38 @@
+const { readFile, writeFile } = require("fs");
+const path = require("path")
+function readFilePromsie(path) {
+    return new Promise((resolve, reject) => {
+        readFile(path, { encoding: "utf-8" }, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    })
+}
+
+function writeFilePromsie(path, data) {
+    return new Promise((resolve, reject) => {
+        writeFile(path, data, { encoding: "utf-8", flag: "a" }, (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve("data written on file ");
+        });
+    })
+}
+
+
+const pathOne = path.resolve(__dirname, "files", "userfiles", "textone.txt");
+const pathFive = path.resolve(__dirname, "files", "userfiles", "textfive.txt");
+readFilePromsie(pathOne).then((data) => {
+    console.log(data);
+    return writeFilePromsie(pathFive, data)
+
+}).then((data) => {
+    console.log(data);
+
+}).catch((err) => {
+    console.log(err);
+
+})
