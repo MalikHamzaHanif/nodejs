db0d688334618b40c800cc61cb24d193af51121d
Hamza Hanif <www.musamalik9@gmail.com>
2025-02-27 02:56:11 +0500

    event loop nodejs

--- 08_event_loop.js
+++ 08_event_loop.js
@@ -1,6 +1,20 @@
+// console.log("task started");
+// setTimeout(() => {
+//     console.log("timeout");
+// }, 0);
+// console.log("task ended");
+
+
+
 console.log("task started");
-setTimeout(() => {
-    console.log("timeout");
-}, 0);
-console.log("task ended");
+const { readFile } = require("fs")
+const path = require("path")
+readFile(path.resolve(__dirname, "files", "userfiles", "textone.txt"), { encoding: "utf-8" }, (err, data) => {
+    console.log(data);
+    console.log("reading file data finished");
+
+})
+
+console.log("task completed");
+
 
